df027936cd9c25859a6b1a187c528001463029c4
XEN <micbeaverstar@gmail.com>
2024-02-12 20:24:49 -0500

    Made main show a blank window to the screen

--- src/main.c
+++ src/main.c
@@ -1,15 +1,26 @@
 #pragma once
+#define _CRT_SECURE_NO_WARNNINGS
 #include <stdlib.h>
 #include <SDL.h>
-
+#include <ctype.h>
 #include "chip8_processor.h"
 #include "chip8_instruction_set.h"
 #include "sdl_config.h"
 
-int main(int argc, char** argv) {
-	uint16_t some_hex_number = 0x1834;
-	uint8_t  myUInt8 = ReadFirstNibble(some_hex_number);
-	sdlVideoStuff();
-	printf("Value of uint8_t in hexadecimal: 0x%02X\n", myUInt8);
+int main(int argc, char** argv){
+	char input = 'a';
+	
+	SDLStart();
+	printf("Press X to close window: ");
+
+	for (;;) {
+		scanf("%c", &input);
+		if (toupper(input) != 'X'){
+			continue;
+		}
+		break;
+	}
+
+	SDLCleanUp();
 	return EXIT_SUCCESS;
 }
--- src/sdl_config.c
+++ src/sdl_config.c
@@ -12,35 +12,25 @@ void SDLStart() {
 }
 // returns 1 if the process wasn't able to compete
 uint8_t SDLInitialize() {
-	SDL_Window* window;
-	SDL_Renderer* renderer;
-	int frameCount, timerFPS, lastFrame, fps;
-
-	if (SDL_Init(SDL_INIT_VIDEO | SDL_INIT_AUDIO) == -1); {
-		printf("[-] SDL could not initialize subsystem: %s.\n", SDL_GetError());
-		return 1;
+	if (SDL_Init(SDL_INIT_VIDEO) < 0) {
+		printf("[-] SDL could not initialize");
 	}
-	printf("[+] SDL Initialized.\n");
-	printf("[i] SDL Setting up video\n");
-	SDLVideo(window);
-	SDLRendrer(renderer, window);
+	SDL_Window* window = 
+		SDL_CreateWindow(CHIP8_APPLICATION_NAME,    SDL_WINDOWPOS_UNDEFINED,
+										 SDL_WINDOWPOS_UNDEFINED,   kChip8DefaultWindowLength,
+										 kChip8DefaultWindowHeight, SDL_WINDOW_SHOWN |
+									   SDL_WINDOW_OPENGL);
 
-	// This function call must happen before any other sdl function is 
-	return 0;
+	SDL_Renderer* renderer = SDL_CreateRenderer(window, -1, 0);
+
+	if (renderer == NULL) {
+		printf("[-] SDL Renderer failted to initialize\n");
+	}
+	SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
+	SDL_RenderClear(renderer);
+	SDL_RenderPresent(renderer);	return 0;
 }
 // Create window for the Chip 8 emulator
-void SDLVideo(SDL_Surface* window) {
-	// Initialzing the values to properties of the window
-	window = SDL_CreateWindow(CHIP8_APPLICATION_NAME,    SDL_WINDOWPOS_UNDEFINED,
-														SDL_WINDOWPOS_UNDEFINED,   kChip8DefaultWindowLength,
-														kChip8DefaultWindowHeight, SDL_WINDOW_FULLSCREEN | 
-														SDL_WINDOW_OPENGL);
-	if (window == NULL) {
-		printf("[-] SDL Window failed to initialize\n");
-		return;
-	}
-	printf("[+] SDL Window initialized .\n");
-}
 
 void SDLUpdate() {
 
@@ -51,13 +41,7 @@ void SDLInput() {
 }
 
 void SDLRendrer(SDL_Renderer* renderer, SDL_Surface* window){
-	renderer = SDL_CreateRenderer(window, -1, 0);
-	if (renderer == NULL) {
-		printf("[-] SDL Renderer failted to initialize\n");
-	}
-	SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
-	SDL_RenderClear(renderer);
-	SDL_RenderPresent(renderer);
+
 }
 
 void SDLDraw() {
